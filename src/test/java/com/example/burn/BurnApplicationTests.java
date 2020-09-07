e6162e63157a3a690ee1838c3e358c95fb2cecf0
javid <cavidxalilov@gmail.com>
2020-09-08 00:59:38 +0300

    initial commit

---
+++ src/test/java/com/example/burn/BurnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.burn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
